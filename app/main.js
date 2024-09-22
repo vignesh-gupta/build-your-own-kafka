4796d90ad3f6f9669c1c3d8e473ec0129f1df3e8
Vignesh Gupta <vighneshgupta32@gmail.com>
2024-09-22 20:51:27 +0530

    move API_VERSION to global as const

--- app/main.js
+++ app/main.js
@@ -1,13 +1,13 @@
 import net from "net";
 
+const API_VERSIONS = [0, 1, 2, 3, 4];
+
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   connection.on("data", (data) => {
     const request_api_key = data.subarray(0, 2);
     const request_api_version = data.subarray(2, 4);
     const correlation_id = data.subarray(4, 12);
-
-    const API_VERSIONS = [0, 1, 2, 3, 4];
 
     if (API_VERSIONS.includes(request_api_version.readUint16BE(0))) {
       console.log(request_api_version.readUint16BE(0));
