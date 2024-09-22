d7102ee64a050590228614fb1109ba8eb8a57fe7
Vignesh Gupta <vighneshgupta32@gmail.com>
2024-09-22 13:24:22 +0530

    add APIVersion request handle

--- app/main.js
+++ app/main.js
@@ -3,6 +3,9 @@ import net from "net";
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   // Handle connection
+  const coorelationID = 7;
+  const response = Buffer.from([0,0,0,0,0,0,0,coorelationID]);
+  connection.write(response);
 });
 
 server.listen(9092, "127.0.0.1");
