eb02e63b35ea2edb99d51836b49a1f045e3534b7
Vignesh Gupta <vighneshgupta32@gmail.com>
2024-09-22 13:44:52 +0530

    parse header data

--- app/main.js
+++ app/main.js
@@ -2,10 +2,13 @@ import net from "net";
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-  // Handle connection
-  const coorelationID = 7;
-  const response = Buffer.from([0,0,0,0,0,0,0,coorelationID]);
-  connection.write(response);
+  connection.on("data", (data) => {
+    const request_api_key = data.subarray(0, 2);
+    const request_api_version = data.subarray(2, 4);
+    const correlation_id = data.subarray(4, 12);
+
+    connection.write(correlation_id);
+  });
 });
 
 server.listen(9092, "127.0.0.1");
