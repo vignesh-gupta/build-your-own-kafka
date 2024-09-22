ed5e85719214397bb370265806a2ce58c5893254
Vignesh Gupta <vighneshgupta32@gmail.com>
2024-09-22 20:47:11 +0530

    add API version check

--- app/main.js
+++ app/main.js
@@ -7,6 +7,19 @@ const server = net.createServer((connection) => {
     const request_api_version = data.subarray(2, 4);
     const correlation_id = data.subarray(4, 12);
 
+    const API_VERSIONS = [0, 1, 2, 3, 4];
+
+    if (API_VERSIONS.includes(request_api_version.readUint16BE(0))) {
+      console.log(request_api_version.readUint16BE(0))
+      
+      connection.write(correlation_id);
+    } else {
+      connection.write(correlation_id);
+      const buffer = Buffer.alloc(2);
+      buffer.writeUint16BE(35);
+      connection.write(buffer);
+    }
+
     connection.write(correlation_id);
   });
 });
